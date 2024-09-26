ab625f26ae1b0b4f59fb9eabb3c673b53b4a0e99
Rahul <159618613+rahulvenkat207@users.noreply.github.com>
2024-09-26 07:12:21 +0530

    Add files via upload

---
+++ js/variable.js
@@ -0,0 +1,20 @@
+// string
+var my_string = "Add text";
+console.log(my_string + "Some more");
+
+// integer
+var myint = 1;
+var myfloat = 1.33;
+
+//boolean
+var yes = true;
+var no = false;
+
+// array
+var arr = ["Rahul","Indian"];
+console.log(arr);
+
+// object 
+var obj = {"name": "Rahul", "Sub": ["Eng","Tamil","Karnata"]};
+console.log(obj.name)
+console.log(obj.Sub)
